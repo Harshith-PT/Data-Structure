df48652a42c87daae960f00346cffa5a7c39aef8
Harshith-PT <ptharshith.cs24@bmsce.ac.in>
2025-11-04 09:23:40 +0530

    Add files via upload

---
+++ infix.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    if (top == MAX - 1)
+        printf("Stack Overflow\n");
+    else
+        stack[++top] = c;
+}
+
+char pop() {
+    if (top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+
+int precedence(char c) {
+    if (c == '+' || c == '-')
+        return 1;
+    else if (c == '*' || c == '/')
+        return 2;
+    else
+        return 0;
+}
+
+void infixToPostfix(char infix[]) {
+    char postfix[MAX];
+    int i, j = 0;
+    char x;
+
+    for (i = 0; i < strlen(infix); i++) {
+        char symbol = infix[i];
+
+        if (isalnum(symbol)) {
+            postfix[j++] = symbol;
+        } else if (symbol == '(') {
+            push(symbol);
+        } else if (symbol == ')') {
+            while ((x = pop()) != '(')
+                postfix[j++] = x;
+        } else {
+            while (precedence(stack[top]) >= precedence(symbol))
+                postfix[j++] = pop();
+            push(symbol);
+        }
+    }
+
+    while (top != -1)
+        postfix[j++] = pop();
+
+    postfix[j] = '\0';
+    printf("Postfix Expression: %s\n", postfix);
+}
+
+int main() {
+    char infix[MAX];
+    printf("Enter a valid infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix);
+    return 0;
+}
